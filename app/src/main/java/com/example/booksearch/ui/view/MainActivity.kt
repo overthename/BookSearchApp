c04ca4aec40ed41ffe84631fdc846216843bd7c3
sia7997 <sia7997@naver.com>
2023-04-16 09:00:01 +0900

    Preferences DataStore로 정렬옵션설정

--- app/src/main/java/com/example/booksearch/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/booksearch/ui/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.booksearch.ui.view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,6 +17,7 @@ import com.example.booksearch.data.repository.BookSearchRepositoryImpl
 import com.example.booksearch.databinding.ActivityMainBinding
 import com.example.booksearch.ui.viewmodel.BookSearchViewModel
 import com.example.booksearch.ui.viewmodel.BookSearchViewModelProviderFactory
+import com.example.booksearch.util.Constants.DATASTORE_NAME
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -24,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var bookSearchViewModel: BookSearchViewModel
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val Context.dataStore by preferencesDataStore(DATASTORE_NAME)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -36,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         setupJetpackNavigation()
 
         val database = BookSearchDatabase.getInstance(this)
-        val bookSearchRepository = BookSearchRepositoryImpl(database)
+        val bookSearchRepository = BookSearchRepositoryImpl(database, dataStore)
         val factory = BookSearchViewModelProviderFactory(bookSearchRepository, this)
         bookSearchViewModel = ViewModelProvider(this, factory)[BookSearchViewModel::class.java]
 
--- app/src/main/java/com/example/booksearch/ui/view/SettingsFragment.kt
+++ app/src/main/java/com/example/booksearch/ui/view/SettingsFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.booksearch.R
 import com.example.booksearch.databinding.FragmentSettingsBinding
+import com.example.booksearch.ui.viewmodel.BookSearchViewModel
+import com.example.booksearch.util.Sort
+import kotlinx.coroutines.launch
 
 class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var bookSearchViewModel: BookSearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +25,36 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
+
+        saveSettings()
+        loadSettings()
+    }
+
+    private fun saveSettings() {
+        binding.rgSort.setOnCheckedChangeListener { _, checkedId ->
+            val value = when (checkedId) {
+                R.id.rb_accuracy -> Sort.ACCURACY.value
+                R.id.rb_latest -> Sort.LATEST.value
+                else -> return@setOnCheckedChangeListener
+            }
+            bookSearchViewModel.saveSortMode(value)
+        }
+    }
+
+    private fun loadSettings() {
+        lifecycleScope.launch {
+            val buttonId = when (bookSearchViewModel.getSortMode()) {
+                Sort.ACCURACY.value -> R.id.rb_accuracy
+                Sort.LATEST.value -> R.id.rb_latest
+                else -> return@launch
+            }
+            binding.rgSort.check(buttonId)
+        }
     }
 
     //필요없을 경우 null처리
